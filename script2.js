21148ad2c86af1de1e66764f3372c7227eb92865
Nicole Miller <nlmiller137@gmail.com>
2013-08-17 14:06:21 -0400

    Final Project Final  Submission

--- script2.js
+++ script2.js
@@ -2,55 +2,48 @@
 
 $(document).ready(function() { 
     
-	$("form").on("submit", function(event) {
-     event.preventDefault();
-	 $('.container').append(html);
+	//Prevents the default action of the form so it doesn't refresh page
+    $("form").on("submit", function(event) {
+        event.preventDefault();
+     });
 
- $("#calc").click(function(){
-        getTime();
+ //On click of submit run getTime function
+    $("#calc").click(function(){
+        $('#result').text(getTime());
+    });
 
 });
 
+var time= 10; //Variable here to test if countdown works since I have having trouble getting the getTime function to work       
 
-});
-	
-});
-
-
-var time= 10;
-var minutes;
-var seconds;
- 		
-/*    
- function getTime()
- {
+//Function that calculates a time variable when the user clicks the submit button
+/*function getTime() {
         var minutes, seconds, time;
-        minutes = (parseInt( $('#mNumber').val(), 10)) * 10000;
-        seconds = (parseInt( $('#sNumber').val(), 10)) * 1000;
+        minutes = (parseInt( $('#mNumber').val(), 10)) * 60;//Turns string to integer
+        seconds = (parseInt( $('#sNumber').val(), 10));//Turns string to integer
         time = minutes + seconds;
-        document.getElementById("timer").innerHTML = time;
+        $('.container').html(time + ' seconds left');//Prints time to page
         return false;
- }	
-*/
-Tick();
+}*/
 
+Tick();//Run the Tick function
 
-$('.container').append(time);
-
+//Function that countsdown the time. Alerts and plays sound when time is up
 function Tick() {
      if (time > 0) {
-	setTimeout(function ()
-	    {
-			$('.container').html(time);
+	setTimeout(function () {
+			$('.container').html(time + ' seconds left');//Prints the time to the page
 			time--;
 			Tick(time);
-		
 		},1000);
     } else {
-		$('.container').html('time is up');
+		$('.container').html('Time is up!');//Prints the message to the page
         PlaySound("sound1");
 	}
 }
 
-
-	
+//Function to play sound
+function PlaySound(soundObj) {
+  var sound = document.getElementById(soundObj);
+  sound.Play();
+}
---
+++ script4.js
@@ -0,0 +1,73 @@
+//Javascript File
+var myTask;
+
+$(document).ready(function() {
+    
+     myTasks = {
+         
+        'tasks' : {
+            'list' : [{
+                'name': 'Laundry',
+                'complete': true
+                },{
+                'name' : 'Homework',
+                'complete' : false
+                }, {
+                'name': 'Gym',
+                'complete': false
+                }, {
+                'name': 'Cook Dinner'
+                
+                }, {
+                'name': 'Shower',
+                'complete': true
+            }],
+            'render': function (task) { 
+                    if(typeof task.name === 'undefined') {
+                    throw 'The name of this task is missing!';
+                } else if (typeof task.complete === 'undefined') {
+                    throw 'The completion status of this task is gone.';
+                }
+    
+                     if(task.complete) {
+                    return '<li class="complete">' + task.name + '</li>';
+                } else {
+                    return '<li>' + task.name + '</li>'; 
+                }
+                }
+            },
+        'add' : function(name) {
+            this.tasks.list.push(new tasks(name));
+        },
+        'remove' : function() {
+            $('.complete').remove();   
+        },
+        'dispay' :function() {
+                var tasks = this.tasks;
+                var html = '<h2>My Tasks</h2>';
+    
+                    html +='<ul>';
+
+                    for(var i= 0; i < tasks.list.length; i++) {
+                        try {
+                            html += tasks.render(tasks.list[i]);
+                        } catch (msg) {
+                            console.log('There is a problem: ' + msg);
+                        }
+                    }
+                    
+                    html += '</ul>';
+                    $('.container').html(html);
+        },
+        'init' : function() {
+            this.add('Finish Project');
+            this.add('Email Boss');
+            this.add('Call Tim');
+            this.display();
+        }
+        
+         
+    };
+});
+
+
